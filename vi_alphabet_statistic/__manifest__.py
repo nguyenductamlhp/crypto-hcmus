fc20781d634915eedeabd81fbf99b28a4b7ce5d9
Nguyen Duc Tam <nguyenductamlhp@gmail.com>
2018-03-11 13:27:55 +0000

    [REF] Refactor code

---
+++ vi_alphabet_statistic/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Vietnamese Alphabet Statistic',
+    'version': '1.0',
+    'category': '',
+    'author': 'nguyenductamlhp@gmail.com',
+    'website': '',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/statistic_vi_view.xml',
+    ],
+
+    'test': [],
+    'demo': [],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+
+    'installable': True,
+    'active': False,
+    'application': True,
+}
